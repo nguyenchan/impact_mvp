799a4b31a61b8d70718ee28640288ab903a28f08
pkcnguyen@gmail.com <pkcnguyen@gmail.com>
2019-01-18 02:51:04 +0700

    add sound

--- src/components/Flashcards/_sheep/FSheep.js
+++ src/components/Flashcards/_sheep/FSheep.js
@@ -3,7 +3,33 @@ import Speaker from "../_icons/Icon_speaker.png";
 import Mic from "../_icons/Icon_microphone.png";
 import SheepPic from "../_icons/sheep_.png";
 import "./FSheep.css";
-import { Icon } from "antd";
+import { Icon, Button } from "antd";
+import SheepSound from "../_sounds/sheep.mp3";
+
+class Speak extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { play: false };
+    this.audio = new Audio(SheepSound);
+    this.togglePlay = this.togglePlay.bind(this);
+  }
+
+  togglePlay() {
+    this.setState({ play: !this.state.play });
+    console.log(this.audio);
+    this.state.play ? this.audio.play() : this.audio.pause();
+  }
+
+  render() {
+    return (
+      <div>
+        <Button shape="circle" onClick={this.togglePlay} className="btn_sheep">
+          <img src={Speaker} alt="speaker" className="FSheep_speak" />
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default function FSheep() {
   return (
@@ -19,7 +45,7 @@ export default function FSheep() {
         <div className="FSheep_btn">
           <img src={Mic} alt="microphone" className="FSheep_record" />
           <div className="blank_space_v" />
-          <img src={Speaker} alt="speaker" className="FSheep_speak" />
+          <Speak />
         </div>
         <div className="blank_space" />
         <div className="blank_space" />
--- src/components/Flashcards/_ship/FShip.js
+++ src/components/Flashcards/_ship/FShip.js
@@ -3,7 +3,33 @@ import Speaker from "../_icons/Icon_speaker.png";
 import Mic from "../_icons/Icon_microphone.png";
 import ShipPic from "../_icons/ship_.png";
 import "./FShip.css";
-import { Icon } from "antd";
+import { Icon, Button } from "antd";
+import ShipSound from "../_sounds/ship.mp3";
+
+class Speak extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { play: false };
+    this.audio = new Audio(ShipSound);
+    this.togglePlay = this.togglePlay.bind(this);
+  }
+
+  togglePlay() {
+    this.setState({ play: !this.state.play });
+    console.log(this.audio);
+    this.state.play ? this.audio.play() : this.audio.pause();
+  }
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.togglePlay} className="btn_ship">
+          <img src={Speaker} alt="speaker" className="FShip_speak" />
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default function FShip() {
   return (
@@ -19,7 +45,7 @@ export default function FShip() {
         <div className="FShip_btn">
           <img src={Mic} alt="microphone" className="FShip_record" />
           <div className="blank_space_v" />
-          <img src={Speaker} alt="speaker" className="FShip_speak" />
+          <Speak />
         </div>
         <div className="blank_space" />
         <div className="blank_space" />
