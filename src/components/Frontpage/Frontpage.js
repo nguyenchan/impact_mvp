b6809cef218af815345ee8c28291793ddf91fcc4
pkcnguyen@gmail.com <pkcnguyen@gmail.com>
2019-01-18 09:19:14 +0700

    change logo

--- src/components/Frontpage/Frontpage.js
+++ src/components/Frontpage/Frontpage.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Frontpage.css";
-import logo from "../../logo.svg";
+import logo from "./logo_.png";
 import { Icon } from "antd";
 
 export default function Frontpage() {
   return (
     <div className="frontpage_wrapper">
-      <span className="_title">Herb</span>
       <img src={logo} className="_logo" alt="logo" />
-      <a href="/sheep">
+      <div className="blank" />
+      <span className="_title">Herb.</span>
+      <a href="/sheep" className="btn_play">
         <Icon type="play-circle" />
       </a>
     </div>
